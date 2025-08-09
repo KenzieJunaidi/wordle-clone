0e1220d758ffd09749781e766c467b4e5bd0aa60
Kenzie Junaidi <kenzie.junaidi2@gmail.com>
2025-08-09 18:33:53 +0700

    Initialize API

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import './App.css'
 
 function App() {
 
+  // Fetch APIs
+  // https://wordle-list.malted.dev/choice -> Pick new word
+  // https://wordle-list.malted.dev/valid?word= -> Validate answer
+
   return (
     <div></div>
   )
